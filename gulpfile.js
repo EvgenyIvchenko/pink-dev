ca9f28a9a4f33efbd12849d989a920a96156a37c
Evgeny <dirath2014@gmail.com>
2024-01-13 19:21:40 +0300

    сборка не работает

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,15 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import webp from 'gulp-webp';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
+import browser, { reload } from 'browser-sync';
 
 // Styles
 
@@ -12,10 +20,37 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// HTML
+
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Script
+
+export const script = () => {
+  return gulp.src('source/js/script.js')
+    .pipe(terser())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('build/js'));
+}
+
+// Images
+
+export const images = () => {
+  return gulp.src('source/img/*. {jpg,png,svg}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'));
 }
 
 // Server
@@ -23,7 +58,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -36,10 +71,10 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, browser.reload));
 }
 
 
 export default gulp.series(
-  styles, server, watcher
+  styles, html, server, watcher
 );
